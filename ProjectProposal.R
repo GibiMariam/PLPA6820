dd84aefbe6c6844e2075d510d727b982d00b7f85
GibiMariam <gmt0035@auburn.edu>
2026-02-05 09:08:19 -0600

    commit

---
+++ ProjectProposal.R
@@ -0,0 +1,25 @@
+#Project proposal
+#Data Source and Description
+#The data that will be used in this project is generated from a controlled rainfall simulation experiment designed to quantify phosphorus (P) losses under different management conditions. The dataset includes measurements of dissolved reactive P (DRP) and total P collected after each rainfall cycle. The experiment consists of 12 treatments arranged in a completely randomized design (CRD) with four replications. All data generated as part of my own experimental research.  
+#Data Volume
+#It contains a total of 144 observations (12 treatments × 4 replications × 3 rainfall cycles). The dataset includes five variables, including rainfall cycles, treatments, replication and two response variables (DRP and TP). 
+#Research question
+#•	How do different treatments influence P losses during rainfall events? 
+#  •	Does the P runoff concentration change after each rainfall cycle?
+#  •	What will be its impact on water quality? 
+#  Data Analysis 
+#•	Conduct two-way ANOVA with treatment as the main effect
+#•	Check interactions (treatment × rainfall cycles)
+#•	Following statistical analysis and packages will be used for this study: 
+#  Analysis 	R library 
+#LS means	emmeans
+#Tukey test	agricolae
+#Letter groups	multicomp
+#Graphical representation	ggplot2
+#Assumption tests: 
+#  Normality: Shapiro test
+#How to turn your data into a reproducible workflow? 
+#  •	Organize data: Arrange the raw data and save the scripts 
+#•	GitHub: Push all the data and scripts into GitHub, commit changes after every step
+#•	Combine, code, results and explanations
+#•	Include plots and statistical outputs
